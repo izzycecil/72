b266ef3fc0f5d150dd85b13816b316c7707922a7
Izzy Cecil <lorr.cecil@gmail.com>
2013-09-06 23:07:37 -0600

    main.py will call an update from pygame.

--- main.py
+++ main.py
@@ -6,18 +6,14 @@ from stackframe import StackFrame, runStack
 def initGraphics():
     pygame.init()
     fpsClock = pygame.time.Clock()
-
     window = pygame.display.set_mod((600,600))
     pygame.display.set_caption('72 --- Death of One, Death of Many')
-    
     return window
     
 def main():
     stack = []
-    
     while runStack(stack):
-        pass
-
+        pygame.display.update()
     return
 
 if __name__=='__main__':
