5928b3d3a0143cdd6d115e67a648c6e6ebc37362
Izzy Cecil <lorr.cecil@gmail.com>
2013-09-07 01:20:48 -0600

    Board can render sprites now!

--- gameplayFrame.py
+++ gameplayFrame.py
@@ -1,3 +1,4 @@
+import pygame
 
 from stackframe import StackFrame
 
@@ -47,14 +48,48 @@ class Player(Creature):
         super(Player, self).__init__(health, strength, position)
 
 
+class Tile(object):
+    def __init__(self, path, contents):
+        self._path    = path
+        self.contents = contents
+        self.image    = pygame.image.load(self._path)
+        
+    def render(self, window, pos):
+        window.blit(self.image, pos)
+
 class Board(object):
+    tileWidth  = 40
+    tileHeight = 40
+
     def __init__(self, dim):
+        self.dim    = dim
+        self.xDim   = dim[0]
+        self.yDim   = dim[1]
         self.spaces = [
-            [False for x in range(0,dim[0])] for x in range(0,dim[1])
+            [None for x in range(0,dim[0])] 
+            for x in range(0,dim[1])
         ]
 
-
+    def renderBoard(self, window):
+        for x in range(0, self.xDim):
+            for y in range(0, self.yDim):
+                if self.spaces[x][y]:
+                    plotx = 10 + x*Board.tileWidth + (y&1)*(Board.tileHeight/2)
+                    ploty = 10 + (y*(Board.tileHeight/2))/2
+                    self.spaces[x][y].render(window, (plotx, ploty))
+                
 
 if __name__=='__main__':
+    pygame.init()
+    fpsClock = pygame.time.Clock()
+    window = pygame.display.set_mode((600,600))
+    pygame.display.set_caption('72 --- gameTest')
+
     b = Board((10,10))
-    
+
+    while True:
+        window.fill(pygame.Color(255,255,255))
+        b.renderBoard(window)
+        pygame.display.update()
+        fpsClock.tick(30)
+        
