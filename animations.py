46e8fb24b65e5a6dbd22219bf98635eebfd0ba71
U-josh-uses\Josh <Josh@josh-uses.(none)>
2013-09-07 23:05:15 -0600

    animations

---
+++ animations.py
@@ -0,0 +1,24 @@
+import pygame, glob
+
+animationDirections = ['UP', 'BACKLEFT', 'LEFT', 'FRONTLEFT', 'FRONT', 'FRONTRIGHT', 'RIGHT', 'BACKRIGHT']
+
+class GameSprite(object):
+    
+    # path like media/mainimations/png/
+    def __init__(self, path, positions=['idle', 'punch', 'walk']):
+        self.frameDict = {}
+    
+        for pos in positions:
+            for dir in animationDirections:
+                self.frameDict.update({pos+'_'+dir:self.getImages(path, pos+'_'+dir)})
+    
+    def getImages(self, path, subPath):
+        surfaces = []
+        
+        for f in glob.glob(path+subPath+"./*.png"):
+            surfaces.append(pygame.image.load(f))
+            
+        return surfaces
+    
+    def getFrames(self,  position='idle', direction=4):
+        return self.frameDict[position+'_'+animationDirections[direction]]
